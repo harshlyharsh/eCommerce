d103be7c9b51b62cf0394d1848f59a319a537d96
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-14 13:50:43 +0530

    ant-design-icons

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import 'antd/dist/antd.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
      <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
