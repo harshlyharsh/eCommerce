7e302e8cca3455c0dd988e659110f6c0a67923bc
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-14 11:14:19 +0530

    routes-and-pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
-const App = () =>(
-<div>
-    <p>react app</p>
-  </div>
-);
+const App = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
+  );
+};
 
 export default App;
