9b1c16e2a610f01e69c8b100f093859b706f7b09
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-16 12:36:38 +0530

    complete registration

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 import Header from "./components/nav/Header";
+import RegisterComplete from "./pages/auth/RegisterComplete";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complete" component={RegisterComplete} />
       </Switch>
     </>
   );
