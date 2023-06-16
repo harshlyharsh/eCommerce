c2de394d7b8fee888ca8461033c86119573e5b42
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-16 22:49:18 +0530

    client folder

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -5,17 +5,33 @@ import {
   SettingOutlined,
   UserOutlined,
   UserAddOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import firebase from "firebase";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
 
+  let dispatch = useDispatch();
+  let history = useHistory();
+
   const handleClick = (e) => {
     // console.log(e.key);
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
   };
 
   return (
@@ -35,6 +51,9 @@ const Header = () => {
       <SubMenu icon={<SettingOutlined />} title="Username">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
       </SubMenu>
     </Menu>
   );
