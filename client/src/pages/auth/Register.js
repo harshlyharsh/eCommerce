592455ced6d4f8ae9cfd3a7a7af4870e7c94a27e
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-17 16:16:56 +0530

    conditional-rendering

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,9 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
-const Register = () => {
+const Register = ({ history }) => {
   const [email, setEmail] = useState("");
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  useEffect(() => {
+    if (user && user.token) history.push("/");
+  }, [user]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -30,9 +37,11 @@ const Register = () => {
         className="form-control"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
+        placeholder="Your email"
         autoFocus
       />
 
+      <br />
       <button type="submit" className="btn btn-raised">
         Register
       </button>
