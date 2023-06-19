a990d3fb3951e652be9f914fb133718c145d7049
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-19 15:27:11 +0530

    routes-autoloading

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { readdirSync } = require("fs");
 require("dotenv").config();
 
 // app
@@ -23,12 +24,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-// route
-app.get("/api", (req, res) => {
-  res.json({
-    data: "hey you hit node API updated",
-  });
-});
+// routes middleware
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 // port
 const port = process.env.PORT || 8000;
