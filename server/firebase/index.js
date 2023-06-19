d19a58cf3f6b48285fa15cd3b5344ca6be99d47a
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-19 20:28:34 +0530

    firebase-auth-check

---
+++ server/firebase/index.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ecommerce-23d9b-default-rtdb.firebaseio.com"
+});
