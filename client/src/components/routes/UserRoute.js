8181c40a90e4d0055dd920bdc72b41b85ac0db3c
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-21 13:51:54 +0530

    redirecting user

--- client/src/components/routes/UserRoute.js
+++ client/src/components/routes/UserRoute.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Route,Link} from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import LoadingToRedirect from './LoadingToRedirect';
 
 const UserRoute = ({children,...rest}) => {
     const {user} = useSelector((state) => ({...state}));
 
     return user && user.token ? (<Route {...rest} render={()=>children}/>) : (
-        <h1 className = "text-dagner">Loading...</h1>
+        <LoadingToRedirect/>
     );
 };
 
