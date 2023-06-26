a45d900949cabb16de0e8288eaa2cb0d37ace095
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-26 14:15:03 +0530

    product update & delete

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,8 @@ import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
 import SubCreate from "./pages/admin/sub/SubCreate";
 import SubUpdate from "./pages/admin/sub/SubUpdate";
 import ProductCreate from "./pages/admin/product/ProductCreate";
+import AllProducts from "./pages/admin/product/AllProducts";
+import ProductUpdate from "./pages/admin/product/ProductUpdate";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -78,6 +80,12 @@ const App = () => {
         <AdminRoute exact path="/admin/sub" component={SubCreate} />
         <AdminRoute exact path="/admin/sub/:slug" component={SubUpdate} />
         <AdminRoute exact path="/admin/product" component={ProductCreate} />
+        <AdminRoute exact path="/admin/products" component={AllProducts} />
+        <AdminRoute
+          exact
+          path="/admin/product/:slug"
+          component={ProductUpdate}
+        />
       </Switch>
     </>
   );
--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -6,3 +6,23 @@ export const createProduct = async (product, authtoken) =>
       authtoken,
     },
   });
+
+export const getProductsByCount = async (count) =>
+  await axios.get(`${process.env.REACT_APP_API}/products/${count}`);
+
+export const removeProduct = async (slug, authtoken) =>
+  await axios.delete(`${process.env.REACT_APP_API}/product/${slug}`, {
+    headers: {
+      authtoken,
+    },
+  });
+
+export const getProduct = async (slug) =>
+  await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
+
+export const updateProduct = async (slug, product, authtoken) =>
+  await axios.put(`${process.env.REACT_APP_API}/product/${slug}`, product, {
+    headers: {
+      authtoken,
+    },
+  });
