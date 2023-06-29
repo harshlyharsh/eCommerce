bc16f75247cc34d5596d714a045c439fac773506
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-06-29 12:56:49 +0530

    add-product-to-local-storage

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -4,10 +4,32 @@ import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import laptop from "../../images/laptop.png";
 import { Link } from "react-router-dom";
 import { showAverage } from "../../functions/rating";
+import _ from "lodash";
 
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+  const handleAddToCart = () => {
+    // create cart array
+    let cart = [];
+    if (typeof window !== "undefined") {
+      // if cart is in local storage GET it
+      if (localStorage.getItem("cart")) {
+        cart = JSON.parse(localStorage.getItem("cart"));
+      }
+      // push new product to cart
+      cart.push({
+        ...product,
+        count: 1,
+      });
+      // remove duplicates
+      let unique = _.uniqWith(cart, _.isEqual);
+      // save to local storage
+      // console.log('unique', unique)
+      localStorage.setItem("cart", JSON.stringify(unique));
+    }
+  };
+
   // destructure
   const { images, title, description, slug, price } = product;
   return (
@@ -30,13 +52,13 @@ const ProductCard = ({ product }) => {
           <Link to={`/product/${slug}`}>
             <EyeOutlined className="text-warning" /> <br /> View Product
           </Link>,
-          <>
+          <a onClick={handleAddToCart}>
             <ShoppingCartOutlined className="text-danger" /> <br /> Add to Cart
-          </>,
+          </a>,
         ]}
       >
         <Meta
-          title={`${title} - ₹${price}`}
+          title={`${title} - $${price}`}
           description={`${description && description.substring(0, 40)}...`}
         />
       </Card>
