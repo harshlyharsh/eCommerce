ddee1a3e7fc13270850d7af0fe4fb9af9abf1df0
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-07-01 20:08:53 +0530

    get-user-cart-frontend

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/user", (req, res) => {
-  res.json({
-    data: "hey you hit user API endpoint",
-  });
-});
+// middlewares
+const { authCheck } = require("../middlewares/auth");
+// controllers
+const { userCart, getUserCart } = require("../controllers/user");
+
+router.post("/user/cart", authCheck, userCart); // save cart
+router.get("/user/cart", authCheck, getUserCart); // get cart
+
+// router.get("/user", (req, res) => {
+//   res.json({
+//     data: "hey you hit user API endpoint",
+//   });
+// });
 
 module.exports = router;
