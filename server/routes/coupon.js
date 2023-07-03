b05a8e25da6e263db6b44a6471e27e4819c02cab
harshlyharsh <107254682+harshlyharsh@users.noreply.github.com>
2023-07-03 12:03:57 +0530

    create-coupon-page

---
+++ server/routes/coupon.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/auth");
+
+// controller
+const { create, remove, list } = require("../controllers/coupon");
+
+// routes
+router.post("/coupon", authCheck, adminCheck, create);
+router.get("/coupons", list);
+router.delete("/coupon/:coupnId", authCheck, adminCheck, remove);
+
+module.exports = router;
+
+
+
